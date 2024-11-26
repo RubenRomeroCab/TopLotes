473bc2eabb0809d661aa40caf7950dff15daf23d
RubenRomeroCab <rubenromerodev@gmail.com>
2024-11-26 21:38:09 +0100

    Formulario de pago
    y ajuste en las rutas y componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,27 +3,24 @@ import { PaleDetailsComponent } from './components/pale-details/pale-details.com
 import { BodyComponent } from './components/body/body.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
 import { PoliticasEnvioComponent } from './components/politicas-envio/politicas-envio.component';
 import { CarritoDetailsComponent } from './components/carrito-details/carrito-details.component';
 import { SubscribeComponent } from './components/subscribe/subscribe.component';
 import { OutletComponent } from './components/categorias/outlet/outlet.component';
 import { HerramientasComponent } from './components/categorias/herramientas/herramientas.component';
 import { InformaticaComponent } from './components/categorias/informatica/informatica.component';
-import { CarritoEndComponent } from './components/carrito-end/carrito-end.component';
+
 
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/body', pathMatch: 'full' },
-    {path:'carritoend',component:CarritoEndComponent},
+    {path: '', redirectTo: '/inicio', pathMatch: 'full' },
     {path:'informatica',component:InformaticaComponent},
     {path:'herramientas',component:HerramientasComponent},
     {path:'outlet',component:OutletComponent},
-    {path:'home',component:HomeComponent},
-    {path:'body',component:BodyComponent},
+    {path:'inicio',component:HomeComponent},
+    {path:'tienda',component:BodyComponent},
     {path:'pale-details/:id',component:PaleDetailsComponent},
     {path:'about',component:AboutComponent},
-    {path:'contacto',component:ContactoComponent},
     {path:'politicas-envio',component:PoliticasEnvioComponent},
     {path:'carrito-details',component:CarritoDetailsComponent},
     {path:'subcribe',component:SubscribeComponent},
--- src/app/components/carrito-details/carrito-details.component.ts
+++ src/app/components/carrito-details/carrito-details.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Route, Router, RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { PaleService } from '../../services/pales.service';
 import { Pale } from '../../models/pale.model';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
@@ -14,7 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-carrito-details',
   standalone: true,
-  imports: [RouterLink, CommonModule,ReactiveFormsModule ,FormsModule],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './carrito-details.component.html',
   styleUrl: './carrito-details.component.css'
 })
@@ -23,17 +23,28 @@ export class CarritoDetailsComponent implements OnInit {
 
   pale !: Pale[]
   precioTotalConIVA: number = 0;
-  
- 
+
+  formData = {
+    nombre: '',
+    apellido: '',
+    nif: '',
+    telefono: null,
+    email: '',
+    poblacion: '',
+    provincia: '',
+    notas: '',
+    paleIds: [] as number[],
+  };
+
   checkoutForm: any;
-  elementoEliminado!:boolean ;
-  paleSelecionado !:number;
+  elementoEliminado!: boolean;
+  paleSelecionado !: number;
   constructor(private carritoService: PaleService,
     private route: Router,
-    private fb: FormBuilder,
 
-  
-    
+
+
+
   ) { }
 
 
@@ -41,37 +52,20 @@ export class CarritoDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.pale = this.carritoService.mostrarcarrtito();
-  
-    this.precioFinalCompra()
+    console.log(this.pale);
 
-  
-      this.checkoutForm = this.fb.group({
-        nombre: ['', Validators.required],
-        apellidos: ['', Validators.required],
-        empresa: [''],
-        nif: ['', Validators.required],
-        pais: ['', Validators.required],
-        direccion: ['', Validators.required],
-        codigoPostal: ['', Validators.required],
-        poblacion: ['', Validators.required],
-        provincia: ['', Validators.required],
-        notas: ['']
-      });
-
-      this.carritoService.getPrecioTotalConIVA$().subscribe({
-        next:(totalConIVA)=>{
-          this.precioTotalConIVA = totalConIVA+(totalConIVA *0.21);
-        }
-
-      })
-      
-  
-     
+    this.precioFinalCompra();
+    this.setPaleIdFromCart();
+    this.carritoService.getPrecioTotalConIVA$().subscribe({
+      next: (totalConIVA) => {
+        this.precioTotalConIVA = totalConIVA + (totalConIVA * 0.21);
+      }
+    })
   }
-  
 
 
-  
+
+
 
   borrarPale(event: Event, idPale: number) {
     event.stopPropagation();
@@ -80,10 +74,10 @@ export class CarritoDetailsComponent implements OnInit {
       // Eliminar el objeto usando splice
       this.pale.splice(index, 1);
     }
-  
+
     // Llama al servicio para eliminar el Pale
     this.carritoService.eliminarPale(idPale);
-  
+
     // Actualiza el precio final
     this.precioFinalCompra();
     this.elementoEliminado = true;
@@ -93,7 +87,7 @@ export class CarritoDetailsComponent implements OnInit {
 
   }
 
-  
+
   verPale(id: number) {
     this.route.navigate(['/pale-details', id])
     console.log('pale seleccionado')
@@ -103,40 +97,57 @@ export class CarritoDetailsComponent implements OnInit {
   precioFinalCompra() {
     // Resetear el precioFinal a cero antes de calcularlo de nuevo
     this.precioTotalConIVA = 0;
-  
+
     // Sumar el precio base de todos los palés
     for (let i = 0; i < this.pale.length; i++) {
       // Asegúrate de que pale[i].precio es un número y suma el precio base
       this.precioTotalConIVA += Number(this.pale[i].precio) || 0;
     }
-    if(this.pale.length>0){
-     
+    if (this.pale.length > 0) {
+
     }
 
   }
 
- 
+  pagoRealizado() {
 
-//  pagoRealizado(){
+    for (let i = 0; i < this.pale.length; i++) {
+      //Asegúrate de que pale[i].precio es un número y suma el precio base
+      this.pale[i].vendido = true;
 
-//    for (let i = 0; i < this.pale.length; i++) {
-      // Asegúrate de que pale[i].precio es un número y suma el precio base
-   //   this.pale[i].vendido=true;
-      
- //   }
-    
- //   this.carritoService.limpiarCarrito()
-    
-    
-////  }
-  
+    }
+
+    this.carritoService.limpiarCarrito()
+
+
+  }
 
 
 
-selectedOption: string = ''; // Almacena la opción seleccionada
 
-onOptionChange(): void {
-  // Se puede agregar lógica adicional si es necesario
-  console.log(`Opción seleccionada: ${this.selectedOption}`);
-}
+  selectedOption: string = ''; // Almacena la opción seleccionada
+
+  onOptionChange(): void {
+    // Se puede agregar lógica adicional si es necesario
+    console.log(`Opción seleccionada: ${this.selectedOption}`);
+  }
+
+  enviar(form: NgForm) {
+
+    if (form.invalid) {
+      console.log("ERROR");
+    } else {
+      console.log(form.value);
+      this.pagoRealizado();
+      form.reset();
+    }
+  }
+
+  setPaleIdFromCart() {
+    if (this.pale.length > 0) {
+      // Si hay palés en el carrito, obtenemos los IDs de todos los palés
+      this.formData.paleIds = this.pale.map(pale => pale.id);  // Extraemos los IDs de los palés
+      console.log('IDs de palés asignados:', this.formData.paleIds);
+    }
+  }
 }
